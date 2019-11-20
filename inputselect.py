6b4f602bc69dd50deff8d9ff0c38fd1287d74fd4
hurui1 <cyhurui@163.com>
2019-11-20 14:58:02 +0800

    add log file to debug

---
+++ inputselect.py
@@ -0,0 +1,24 @@
+import os
+import tkinter.filedialog
+
+def selectbyUser():
+    while(1):
+        list = []
+        select = int(input())
+        print(select)
+        if select == "\n" :
+            break
+        elif select == "\r" :
+            list.append(select)
+    return list
+
+
+def showlist(mlist):
+    # 文件对话框：
+    for mlist_temp in mlist:
+       print(mlist_temp)
+
+
+mlist = ["a","b","c","d","e","f"]
+showlist(mlist)
+print(selectbyUser())
